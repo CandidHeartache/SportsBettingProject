d1cd06283376f581e36761a4fcf907a91f058c78
CandidHeartache <michael.c.norris@gmail.com>
2020-08-16 15:30:23 +1000

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -7,6 +7,13 @@ Created on Sun Aug 16 12:56:55 2020
 
 import loginscript as ls
 import pandas as pd
+
+import datetime
+
+import betfairlightweight as bfl
+from betfairlightweight import filters
+
+
 trading = ls.trading
 
 event_types = trading.betting.list_event_types()
@@ -17,3 +24,41 @@ sport_ids = pd.DataFrame({
 }).set_index('Sport').sort_index()
 
 sport_ids
+
+##############################################################################
+# Finding the Event Type ID for Politics
+
+#making a filter for just Politics
+politics_filter = bfl.filters.market_filter(text_query='Politics')
+
+# Returns a list using the filter
+politics_event_type = trading.betting.list_event_types(filter=politics_filter)
+
+# return first element in list
+politics_event_type = politics_event_type[0]
+
+politics_event_type_id = politics_event_type.event_type.id
+print(f"the event type id for Politics is {politics_event_type_id}")
+
+
+##############################################################################
+# Seems to be defining a time variable then asking for the at datetime_in_a_week
+datetime_in_a_week = (datetime.datetime.utcnow() + datetime.timedelta(weeks=1)).strftime("%Y-%m-%dT%TZ")
+# creating a competition filter
+competition_filter = bfl.filters.market_filter(
+    # event_type_ids=politics_event_type_id,
+    event_type_ids=[1],
+    market_start_time={
+        'to':datetime_in_a_week
+    })
+
+#get a list of competitions for Politics
+competition = trading.betting.list_competitions(
+    filter=competition_filter)
+
+
+
+
+
+
+
